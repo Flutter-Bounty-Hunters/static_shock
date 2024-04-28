4b9ab4f370f8d98655409b8872241e3b6c9f0074
Matt Carroll <matthew-carroll@users.noreply.github.com>
2024-04-28 15:13:09 -0700

    Updated main package file to export all source files (at least one was missing)

--- packages/static_shock/lib/static_shock.dart
+++ packages/static_shock/lib/static_shock.dart
@@ -1,21 +1,22 @@
 library static_shock;
 
 // ----- Core constructs -----
-export 'src/static_shock.dart';
-export 'src/pipeline.dart';
-export 'src/pages.dart';
 export 'src/assets.dart';
-export 'src/finishers.dart';
-export 'src/templates/layouts.dart';
-export 'src/templates/components.dart';
-export 'src/source_files.dart';
+export 'src/data.dart';
 export 'src/files.dart';
+export 'src/finishers.dart';
+export 'src/pages.dart';
+export 'src/pipeline.dart';
+export 'src/source_files.dart';
+export 'src/static_shock.dart';
+export 'src/templates/components.dart';
+export 'src/templates/layouts.dart';
 
 // ----- Plugins -----
 export 'src/plugins/drafting.dart';
+export 'src/plugins/jinja.dart';
 export 'src/plugins/markdown.dart';
 export 'src/plugins/menus.dart';
-export 'src/plugins/jinja.dart';
 export 'src/plugins/pretty_urls.dart';
 export 'src/plugins/pub_package.dart';
 export 'src/plugins/rss.dart';
